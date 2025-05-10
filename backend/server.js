7b654322b6cb7f47a47aa0541256ce2b9e30fc8e
codesaffi <saffi8946@gmail.com>
2025-05-10 17:10:06 +0500

    landing page

--- backend/server.js
+++ backend/server.js
@@ -33,5 +33,5 @@ app.get('/', (req, res) => {
   res.send('Pharmacy API is running...');
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
