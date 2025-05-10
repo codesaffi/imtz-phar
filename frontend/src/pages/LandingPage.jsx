02b4619fa24e2b62608325da5d1e1a10c6ec5d9d
codesaffi <saffi8946@gmail.com>
2025-05-10 12:29:52 +0500

    website change

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
-import Sidebar from '../components/SideBar';
+
+import Sidebar from '../components/Sidebar';
 import { Outlet } from 'react-router-dom';
 import classNames from 'classnames';
 
